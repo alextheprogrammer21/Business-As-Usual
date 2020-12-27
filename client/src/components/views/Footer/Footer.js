72e549c16c07fa1d2acc3284ee620e295f905c8b
Alex Setia <alextheprogrammer21@gmail.com>
2020-12-27 18:13:43 -0500

    initial commit

---
+++ client/src/components/views/Footer/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Footer.css";
+import { SmileTwoTone } from "@ant-design/icons";
+
+function Footer() {
+  return (
+    <footer style={{ textAlign: "center", padding: "1rem" }}>
+      Footer <SmileTwoTone />
+    </footer>
+  );
+}
+
+export default Footer;
