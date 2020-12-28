1b41515829c930c24a14270830d28987653082cf
Alex Setia <alextheprogrammer21@gmail.com>
2020-12-28 13:36:36 -0500

    Add tab bar with three items

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,4 +1,4 @@
-import React, { Suspense } from 'react';
+import React, { Suspense } from "react";
 import { Route, Switch } from "react-router-dom";
 import Auth from "../hoc/auth";
 // pages for this product
@@ -7,24 +7,30 @@ import Auth from "../hoc/auth";
 import LandingPage from "./views/LandingPage/LandingPage.js";
 import LoginPage from "./views/LoginPage/LoginPage.js";
 import RegisterPage from "./views/RegisterPage/RegisterPage.js";
+import Taskbar from "./Taskbar.js";
 
 import NavBar from "./views/NavBar/NavBar";
 import Footer from "./views/Footer/Footer";
 
 //About User
-import NotFoundPage from './views/NotFoundPage/NotFoundPage';
+import NotFoundPage from "./views/NotFoundPage/NotFoundPage";
 
 function App() {
   return (
-    <Suspense fallback={(<div>Loading...</div>)}>
+    <Suspense fallback={<div>Loading...</div>}>
       <div className="wrapper">
         <NavBar />
         <div className="contentsWrapSpacer" />
+        <Taskbar></Taskbar>
         <Switch>
           <div className="contentsWrap">
             <Route exact path="/" component={Auth(LandingPage, null)} />
             <Route exact path="/login" component={Auth(LoginPage, false)} />
-            <Route exact path="/register" component={Auth(RegisterPage, false)} />
+            <Route
+              exact
+              path="/register"
+              component={Auth(RegisterPage, false)}
+            />
           </div>
           <Route component={Auth(NotFoundPage, null)} />
         </Switch>
