f57300aadae20d3fb603f1998e1704fd60093b64
Alex Setia <alextheprogrammer21@gmail.com>
2020-12-29 22:33:59 -0500

    Have dueDate show the value of a calendar selected date

--- client/src/components/Taskbar.js
+++ client/src/components/Taskbar.js
@@ -3,6 +3,7 @@ import { makeStyles } from "@material-ui/core/styles";
 import Paper from "@material-ui/core/Paper";
 import Tabs from "@material-ui/core/Tabs";
 import Tab from "@material-ui/core/Tab";
+import DueDate from "./Tasks/DueDate.js";
 
 const useStyles = makeStyles({
   root: {
@@ -28,6 +29,7 @@ export default function Taskbar(props) {
         textColor="primary"
         centered
       >
+        <DueDate />
         <Tab label={items.task} />
         <Tab label={items.person} />
         <Tab label={items.status} />
