e79e7dcba7f42fa9033f2b2c42d8a7e67261f2c1
Alex Setia <alextheprogrammer21@gmail.com>
2021-01-01 12:16:29 -0500

    clean up boilerplate to make project our own

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,16 +1,5 @@
-import React, { Suspense } from "react";
+import React from "react";
 import { Route, Switch } from "react-router-dom";
-import Auth from "../hoc/auth";
-// pages for this product
-
-//About Auth
-import LandingPage from "./views/LandingPage/LandingPage.js";
-import LoginPage from "./views/LoginPage/LoginPage.js";
-import RegisterPage from "./views/RegisterPage/RegisterPage.js";
-import Taskbar from "./Taskbar.js";
-
-//About User
-import NotFoundPage from "./views/NotFoundPage/NotFoundPage";
 
 const TaskItems = {
   task: "Test task",
@@ -20,26 +9,12 @@ const TaskItems = {
   estimatedTime: "5 hours",
   timeTracking: "4:59",
 };
+
 function App() {
   return (
-    <Suspense fallback={<div>Loading...</div>}>
-      <div className="wrapper">
-        <div className="contentsWrapSpacer" />
-        <Switch>
-          <div className="contentsWrap">
-            <Route exact path="/" component={Auth(LandingPage, null)} />
-            <Taskbar items={TaskItems} />
-            <Route exact path="/login" component={Auth(LoginPage, false)} />
-            <Route
-              exact
-              path="/register"
-              component={Auth(RegisterPage, false)}
-            />
-          </div>
-          <Route component={Auth(NotFoundPage, null)} />
-        </Switch>
-      </div>
-    </Suspense>
+    <div>
+      <h1>Hi</h1>
+    </div>
   );
 }
 
